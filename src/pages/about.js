3366e7e6b3b586eb4510cb25316a4f36ef9eb535
Billy Park <billyparkweb@gmail.com>
2018-04-05 14:33:30 +0100

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About Us</h1>
+                <p>
+                    Pitchfork live-edge poutine godard knausgaard. Synth you probably haven't heard of them pour-over tumblr echo park. Swag cliche chambray air plant, echo park lumbersexual kitsch chartreuse vaporware forage asymmetrical health goth. Intelligentsia glossier slow-carb shaman, hoodie pug man bun bicycle rights marfa. Tattooed keffiyeh humblebrag drinking vinegar vice. Fashion axe narwhal air plant kitsch.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ src/posts/post.md
@@ -0,0 +1,12 @@
+---
+title: "Welcome to the Wizzard Tower! (cool name for a blog right?)"
+date: "2018-04-04"
+---
+
+This is just going to be a test post, click above to see more...
+
+<!-- end -->
+  
+Awesome, it looks like it works :)
+  
+\o/
